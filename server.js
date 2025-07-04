31bbf521ebe2e24ea95d5ee41d5baf69bb6a821c
Amrit Pant <info.amritpant@gmail.com>
2025-07-04 07:57:12 +0545

    Added & Configured .env file

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+const dotenv = require("dotenv");
+
+//   Confituring .envfile
+dotenv.config();
+
 const express = require("express");
 
-console.log("Hello How are you");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("First API Request");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is Running on Port 3001");
+});
