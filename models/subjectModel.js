e4e25d0784ea86a0be5852c82cf38c5e8643c835
Amrit Pant <info.amritpant@gmail.com>
2025-07-04 08:49:47 +0545

    Added connection with database and made subject  model

---
+++ models/subjectModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+// For topic Schema
+const topicSchema = new mongoose.Schema({
+  tittle: {
+    type: String,
+    require: [true, "Please Provide the Name of the Topic"],
+    minLength: [5, "A topic can't be less than 5 letters"],
+    maxLength: [30, "A topic can't be more than 30 letters"],
+    unqiue: [true, "A topic must be unqiue"],
+  },
+  weightage: {
+    type: Number,
+    require: [true, "Please Provide the Weightage of the Topic"],
+    min: [0, "A weightage of the topic can't be less than 0"],
+    max: [100, "A weightage of the topic can't be more than 100"],
+  },
+});
+
+const subjectSchema = new mongoose.Schema({
+  tittle: {
+    type: String,
+    require: [true, "Please Provide Name/Tittle of the Subject"],
+    minLength: [3, "A Subject Name can't be less then 3 letters"],
+    maxLength: [15, "A subject Name can't be more then 15 letters"],
+    unqiue: true,
+  },
+  weightage: {
+    type: Number,
+    require: [true, "Please Provide the Weightage of the Subject"],
+    min: [0, "A weightage can't be less than 0 "],
+    max: [100, "A weightage can't be more than 100"],
+  },
+  topic: {
+    type: [topicSchema],
+    required: [true, "Please Provide the topic of the subject"],
+  },
+});
+
+const Subject = mongoose.Model("Subject", subjectSchema);
+module.exports = Subject;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 //   Confituring .envfile
 dotenv.config();
@@ -6,6 +7,18 @@ dotenv.config();
 const express = require("express");
 
 const app = express();
+
+//  Connecting to a Database
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.txseqeh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then((e) => {
+    console.log(`Database Connected Sucessfully ${e}`);
+  })
+  .catch(() => {
+    console.error(`Some unexpected Error Occured`);
+  });
 
 app.get("/", (req, res) => {
   res.send("First API Request");
